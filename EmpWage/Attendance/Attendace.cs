707b61ec6786a1a25b69b300208a05c8371748ff
unknown <hindhujhasridhar@gmail.com>
2021-12-13 19:44:16 +0530

    CHECKING ATTENDANCE

---
+++ EmpWage/Attendance/Attendace.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage.Attendance
+{
+    internal class Attendace
+    {
+        const int IS_FULL_TIME = 1;
+        int empCheck;
+        public void Present()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+        }
+        public void Empcheck()
+        { 
+    
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+            }
+            else
+            {
+                Console.WriteLine("EMPLOYEE IS ABSENT");
+            }
+        }
+}
+    
+
+}
+
--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -6,6 +6,30 @@ namespace EmpWage
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMS");
+          
+            while (true)
+            {
+                Console.WriteLine("-----------------");
+                Console.WriteLine("1.EMPLOYEES ATTENDANCE");
+                
+                Console.WriteLine("ENTER YOUR OPTION");
+
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                        Attendance.Attendace check = new Attendance.Attendace();
+                        check.Present();
+                        check.Empcheck();
+                        break;
+                   
+                    default:
+                        Console.WriteLine("ENTER CORRECT OPTION: ");
+                        break;
+                }
+            }
         }
     }
-}
+    }
+
