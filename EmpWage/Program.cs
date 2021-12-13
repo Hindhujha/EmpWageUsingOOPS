7e6e22d05f38dc4240c8b6b96853e377f242869e
unknown <hindhujhasridhar@gmail.com>
2021-12-13 21:01:58 +0530

    Main Program

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -11,7 +11,9 @@ namespace EmpWage
             {
                 Console.WriteLine("-----------------");
                 Console.WriteLine("1.EMPLOYEES ATTENDANCE");
-                
+                Console.WriteLine("2.DAILY WAGE OF EMPLOYEES");
+                Console.WriteLine("3.PART TIME EMPLOYEE WAGE ADDED");
+                Console.WriteLine("4.USING SWITCH CASE");
                 Console.WriteLine("ENTER YOUR OPTION");
 
                 int select = Convert.ToInt32(Console.ReadLine());
@@ -23,7 +25,21 @@ namespace EmpWage
                         check.Present();
                         check.Empcheck();
                         break;
-                   
+                    case 2:
+                        DailyWage.Daily_Wage wage = new DailyWage.Daily_Wage();
+                        wage.Wage();
+                        wage.Time();
+                        break;
+                    case 3:
+                        PartTime.Part_Time myObj = new PartTime.Part_Time();
+                        myObj.Parttime();
+                        myObj.wage();
+                        break;
+                    case 4:
+                        SwitchCase.Switch_Case employee = new SwitchCase.Switch_Case();
+                        employee.Case();
+                        employee.Empwage();
+                        break;
                     default:
                         Console.WriteLine("ENTER CORRECT OPTION: ");
                         break;
